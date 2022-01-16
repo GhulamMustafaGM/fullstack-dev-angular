17fe1aee8f6dd47b6bdfbdd7f0c71c410a8268d5
Ghulam Mustafa <mustafaji@gmail.com>
2022-01-16 03:57:16 +0100

    worked on meal tracker frontend

--- meal-tracker-frontend/src/app/home-page/home-page.component.ts
+++ meal-tracker-frontend/src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IngredientsService } from '../ingredients.service';
+import { MealsService } from '../meals.service';
+import { Ingredient, Meal } from '../types';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  isLoadingMeals = true;
+  meals: Meal[] = [];
 
-  constructor() { }
+  isLoadingIngredients = true;
+  ingredients: Ingredient[] = [];
+
+  constructor(
+    private mealsService: MealsService,
+    private ingredientsService: IngredientsService,
+  ) { }
 
   ngOnInit(): void {
+    this.mealsService.getMeals()
+      .subscribe(meals => {
+        this.meals = meals;
+        this.isLoadingMeals = false;
+      });
+    this.ingredientsService.getIngredients()
+      .subscribe(ingredients => {
+        this.ingredients = ingredients;
+        this.isLoadingIngredients = false;
+      });
   }
 
+  onDeleteMeal(mealId: string) {
+    this.mealsService.deleteMeal(mealId)
+      .subscribe(updatedMeals => {
+        this.meals = updatedMeals;
+      });
+  }
+
+  onDeleteIngredient(ingredientName: string) {
+    this.ingredientsService.deleteIngredient(ingredientName)
+      .subscribe(updatedIngredients => {
+        this.ingredients = updatedIngredients;
+      });
+  }
 }
